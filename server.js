2934efbbae45c865112db45783996ff2bd77ab8f
DenisImamovicc <Danielarbete@protonmail.com>
2022-12-03 14:41:14 +0100

    Add handling of data from 3rd party and markin req from client id as ready to respond to client

--- server.js
+++ server.js
@@ -3,24 +3,35 @@ const fetch = require('node-fetch')
 
 const app = express()
 const port = 3000
-const ClientsDb = []
+const clientReqs = []
 
 app.use(express.json())
 app.post('/client/:id', (req, res) => {
   const clientId = req.params.id
-  ClientsDb.push({"clientId":clientId,"Data":null,isCompleted:false})
+  clientReqs.push({ "clientId": clientId, "Data": null, isCompleted: false })
 
 
 
-  console.log(ClientsDb );
+  console.log(clientReqs);
   //res.send({}).status(200)
-  })
+})
+
 
 app.post('/webhook/:id', (req, res) => {
-  console.log(`recieved webhook from client ${req.params.id}`,req.body);
-  
+  console.log(`recieved webhook from client ${req.params.id}`, req.body);
+  const data = req.body.result
+  const webhookId = req.params.id
+
+  clientReqs.forEach(clientReq => {
+    if (webhookId === clientReq.clientId) {
+      clientReq.Data = data
+      clientReq.isCompleted = true
+    }
+  })
+
+  console.log(clientReqs);
   res.status(200).end()
-  })  
+})
 
 app.listen(port, () => {
   console.log(port, `Live at http://localhost:${port}`)
