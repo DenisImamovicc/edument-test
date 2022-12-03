bae8ee13adc98f3e695bdff15224d2debbf4378c
DenisImamovicc <Danielarbete@protonmail.com>
2022-12-03 14:13:42 +0100

    create client obj to wait for webhook notes.

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ const fetch = require('node-fetch')
 
 const app = express()
 const port = 3000
+const ClientsDb = []
 
-app.use( express.json())
+app.use(express.json())
 app.post('/client/:id', (req, res) => {
-  const {id} = req.params
-  console.log( 'revcieved client req',id );
-  res.send({}).status(200)
+  const clientId = req.params.id
+  ClientsDb.push({"clientId":clientId,"Data":null,isCompleted:false})
+
+
+
+  console.log(ClientsDb );
+  //res.send({}).status(200)
   })
 
 app.post('/webhook/:id', (req, res) => {
-  const {id} = req.params
-  console.log( 'revcieved client req',id );
-  res.send({ "result": "ok" })
-      
-    })  
+  console.log(`recieved webhook from client ${req.params.id}`,req.body);
+  
+  res.status(200).end()
+  })  
 
 app.listen(port, () => {
   console.log(port, `Live at http://localhost:${port}`)
