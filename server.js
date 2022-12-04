29d439ea2c4e21b85bc3eeef0215f653c7487cd0
DenisImamovicc <Danielarbete@protonmail.com>
2022-12-04 14:34:29 +0100

    Add temporary simulation of waiting for webhook res to req to client by literally stopping code for x seconds.

--- server.js
+++ server.js
@@ -5,32 +5,48 @@ const app = express()
 const port = 3000
 const clientReqs = []
 
-app.use(express.json())
-app.post('/client/:id', (req, res) => {
-  const clientId = req.params.id
-  clientReqs.push({ "clientId": clientId, "Data": null, isCompleted: false })
-
-
-
-  console.log(clientReqs);
-  //res.send({}).status(200)
-})
-
-
-app.post('/webhook/:id', (req, res) => {
-  console.log(`recieved webhook from client ${req.params.id}`, req.body);
+const getDataFrom3rdParty = () => clientReqs.splice(0, 1);
+ 
+async function match3rdPartyRes(req) {
   const data = req.body.result
   const webhookId = req.params.id
 
-  clientReqs.forEach(clientReq => {
+  return clientReqs.forEach(clientReq => {
     if (webhookId === clientReq.clientId) {
       clientReq.Data = data
       clientReq.isCompleted = true
     }
   })
 
-  console.log(clientReqs);
-  res.status(200).end()
+}
+
+app.use(express.json())
+app.post('/client/:id', (req, res) => {
+  const clientId = req.params.id
+  clientReqs.push({ "clientId": clientId, "Data": null, isCompleted: false })
+
+//Simulate waiting for webhook notes. 
+  setTimeout(() => {
+    try {
+      res.send(getDataFrom3rdParty()).status(200)
+    } catch (error) {
+      res.send(error).status(404)
+    }
+ 
+   console.log(clientReqs);
+  }, "10000")
+})
+
+app.post('/webhook/:id', (req, res) => {
+  console.log(`recieved webhook from client ${req.params.id}`, req.body);
+  match3rdPartyRes(req)
+
+  try {    
+    res.status(200).end()
+  } catch (error) {
+    console.log(error);
+    res.status(404).end()
+  }
 })
 
 app.listen(port, () => {
