98ec1dcb5fd5bada69655c6d8fefb4ab2248b4bb
DenisImamovicc <Danielarbete@protonmail.com>
2022-12-06 17:26:25 +0100

    Fix critical bug for res handling more than one req from 3rd party

--- server.js
+++ server.js
@@ -1,35 +1,30 @@
 const express = require('express')
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
-const { getClientStoredReq,updateClientReq,createClientReq,isMatchingId } = require("./utils/functions");
+const { getClientStoredReq, updateClientReq, createClientReq, isMatchingId } = require("./utils/functions");
 
 const app = express()
 const port = 3000
 
-
 app.use(express.json())
 app.post('/client/:id', (req, res) => {
   createClientReq(req)
-  eventEmitter.on('3rdPartyRes', () => {
+  eventEmitter.once('3rdPartyRes', () => {
     try {
-      res.send(getClientStoredReq()).status(200)
+      return res.send(getClientStoredReq()).status(200)
     } catch (error) {
-      res.send(error).status(404)
+      return res.send(error).status(404)
     }
   });
 })
 
 app.post('/webhook/:id', async (req, res) => {
-  try {
-    if (isMatchingId(req)) {
-      updateClientReq(req)
-      eventEmitter.emit('3rdPartyRes')
-      return res.sendStatus(200)
-    }
-    res.sendStatus(404).send(error.message)
-  } catch (error) {
-    console.log(error.message)
+  if (isMatchingId(req)) {
+    updateClientReq(req)
+    eventEmitter.emit('3rdPartyRes')
+    return res.sendStatus(200)
   }
+  return res.sendStatus(404)
 })
 
 app.listen(port, () => {
--- utils/functions.js
+++ utils/functions.js
@@ -4,18 +4,20 @@ const getClientStoredReq = () => clientReqsStorage.splice(0, 1);
 
 const updateClientReq = (req) => {
     const webhookData = req.body.result
-    clientReqsStorage[0].Data = webhookData
-    clientReqsStorage[0].isCompleted = true
+    return clientReqsStorage[0].Data = webhookData
 }
 
 const createClientReq = (req) => {
     const clientId = req.params.id
-    return clientReqsStorage.push({ "clientId": clientId, "Data": null, isCompleted: false })
+    return clientReqsStorage.push({ "clientId": clientId, "Data": null })
 }
 
 const isMatchingId = (req) => {
     const webhookId = req.params.id
-    return clientReqsStorage.some(clientReq => webhookId === clientReq.clientId)
+    if (webhookId === clientReqsStorage[0].clientId) {
+        return true
+    }
+    return false
 }
 
 module.exports = {
