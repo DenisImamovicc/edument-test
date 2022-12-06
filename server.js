00f14e819b96d08aedecc12f01bd62d2b19d10db
DenisImamovicc <Danielarbete@protonmail.com>
2022-12-06 14:05:08 +0100

    separate funcs in their own file and remove unused dependencies

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
 const express = require('express')
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
+const { getClientStoredReq,updateClientReq,createClientReq,isMatchingId } = require("./utils/functions");
 
 const app = express()
 const port = 3000
 
-const clientReqsStorage = []
-
-const getClientStoredData = () => clientReqsStorage.splice(0, 1);
-
-const updateClientReq = (req) => {
-  const webhookData = req.body.result
-  clientReqsStorage[0].Data = webhookData
-  clientReqsStorage[0].isCompleted = true
-}
-
-const createClientReq = (req) => {
-  const clientId = req.params.id
-  return clientReqsStorage.push({ "clientId": clientId, "Data": null, isCompleted: false })
-}
-
-const isMatchingId = (req) => {
-  const webhookId = req.params.id
-  return clientReqsStorage.some(clientReq => webhookId === clientReq.clientId)
-}
 
 app.use(express.json())
 app.post('/client/:id', (req, res) => {
   createClientReq(req)
   eventEmitter.on('3rdPartyRes', () => {
     try {
-      res.send(getClientStoredData()).status(200)
+      res.send(getClientStoredReq()).status(200)
     } catch (error) {
       res.send(error).status(404)
     }
@@ -49,6 +31,7 @@ app.post('/webhook/:id', async (req, res) => {
     console.log(error.message)
   }
 })
+
 app.listen(port, () => {
   console.log(port, `Live at http://localhost:${port}`)
 })
